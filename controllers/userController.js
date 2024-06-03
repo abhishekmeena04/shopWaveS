851801b34258ea27cc5dee520bbdc52f4ae7b16a
Abhishek Meena <abhishekmeena7452@gmail.com>
2024-06-03 07:01:44 +0530

    logoutController-updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -89,15 +89,15 @@ const loginController = async (req, res) => {
   }
 };
 
-// const logoutController = async (req, res) => {
-//   return res
-//     .cookie("token", "", {
-//       httpOnly: true,
-//       secure: true,
-//       expires: new Date(0),
-//     }) // to remove cookies from browser cookies
-//     .status(200)
-//     .send({ success: true, message: "Logout Successfull" });
-// };
+const logoutController = async (req, res) => {
+  return res
+    .cookie("token", "", {
+      httpOnly: true,
+      secure: true,
+      expires: new Date(0),
+    }) // to remove cookies from browser cookies
+    .status(200)
+    .send({ success: true, message: "Logout Successfull" });
+};
 
 export { registerController, loginController, logoutController };
