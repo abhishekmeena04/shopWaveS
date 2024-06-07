ca619dab35a978caa91f4cdf5e6e7efd2be8af90
Abhishek Meena <abhishekmeena7452@gmail.com>
2024-06-07 06:55:44 +0530

    middleware-dimmy

--- controllers/userController.js
+++ controllers/userController.js
@@ -93,11 +93,21 @@ const logoutController = async (req, res) => {
   return res
     .cookie("token", "", {
       httpOnly: true,
-      // secure: true,
+      secure: true,
       expires: new Date(0),
     }) // to remove cookies from browser cookies
     .status(200)
     .send({ success: true, message: "Logout Successfull" });
+};
+const middleware = async (req, res) => {
+  return res
+    .cookie("token", "", {
+      httpOnly: true,
+      secure: true,
+      expires: new Date(0),
+    }) 
+    .status(200)
+    .send({ success: true, message: "middleware created" });
 };
 
 export { registerController, loginController, logoutController };
